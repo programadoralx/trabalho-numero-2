4574dbaf8cf9678df86fad8830357b41fc1e1fe9
programadoralx <112201228+programadoralx@users.noreply.github.com>
2022-09-23 14:53:21 -0300

    Add files via upload

---
+++ trabalho de poo/src/questao1/primeira.java
@@ -0,0 +1,26 @@
+package questao1;
+import java.util.Scanner;
+public class primeira {
+
+	public primeira() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int num, poi;
+    Scanner in = new Scanner (System.in);
+     System.out.print("digite um numero");
+      num = in.nextInt();
+      poi = (num%2);
+      if (poi==0) {
+    	 System.out.println ("O número "+num+" é par");
+      } else {
+    	 System.out.println ("O número "+num+" é impar");
+    	 
+      }   
+      
+      
+	
+	}
+}
