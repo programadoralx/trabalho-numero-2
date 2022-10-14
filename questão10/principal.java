b4032b277a9a30b19ecfa6c293d7e13789ed584e
samarafernandes <112201228+programadoralx@users.noreply.github.com>
2022-10-14 16:45:43 -0300

    Add files via upload

---
+++ questão10/principal.java
@@ -0,0 +1,18 @@
+package questão10;
+
+public class principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Tabuada de multiplicação");
+		System.out.print("  ");
+		for (int i = 1; i <= 10; i++){ 
+			System.out.println("\nTabuada do número "+i);  
+			
+			for (int n = 1; n <= 10; n++){
+			System.out.print(i+" x "+n+" = " +i*n);
+			System.out.println("                             ");
+	}
+		}
+}
+ } 
