27cd40336bf49c1facb3222a95270cfbbc806b65
samarafernandes <112201228+programadoralx@users.noreply.github.com>
2022-10-14 16:55:22 -0300

    Add files via upload

---
+++ questão06/principal.java
@@ -0,0 +1,22 @@
+package questão06;
+import java.util.Scanner;
+public class principal {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Vamos descobrir se um número é primo:");
+		Scanner n1 = new Scanner(System.in);
+		System.out.print("Digite um número:");
+		int num1 = n1.nextInt(); 
+		
+		if ((num1 / num1 == 1) && (num1 / 1 == num1)) {
+		System.out.println("O número " + num1 + " é primo!");
+			}
+		else { 
+		System.out.println("O número " + num1 + " não é primo!");
+		}
+		n1.close();	
+
+	}
+
+}
